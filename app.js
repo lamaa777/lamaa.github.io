b4fa8cad18f5d82f106d8ef82083997140f76730
lamaa777 <111194480+lamaa777@users.noreply.github.com>
2022-09-11 16:17:28 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// Код для меню//
+
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(1);
+const body = document.body;
+
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    hamb.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    renderPopup();
+}
+
+function renderPopup() {
+    popup.appendChild(menu);
+}
+const links = Array.from(menu.children);
+
+links.forEach((link) => {
+    link.addEventListener("click", closeOnClick);
+});
+
+function closeOnClick() {
+    popup.classList.remove("open");
+    hamb.classList.remove("active");
+    body.classList.remove("noscroll");
+}
+//Код для адаптивного хедера//
+
+VANTA.RINGS({
+    el: "#header",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 800.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    backgroundColor: 0xffffff,
+    color: 0x0,
+});
